d63ff14db52e9584b286cd84aa7625f25d807b5f
Selina Deep Kaur <selinadeepkaur@Selinas-MacBook-Pro.local>
2017-12-22 03:58:29 -0500

    external data set

--- src/extract_files.py
+++
@@ -1,8 +0,0 @@
-import glob
-import os
-import subprocess
-
-for d in os.listdir("./"):
-	zip_files = glob.glob(d+"/*.zip")
-	for f in zip_files:
-		subprocess.call("unzip "+f+" && rm "+f, shell=True)
--- src/timestamp.py
+++
@@ -1,6 +0,0 @@
-from datetime import datetime, timedelta
-import time
-
-t = 1326920388
-dt = datetime.fromtimestamp(t) + timedelta(days=14)
-print(int(time.mktime(dt.timetuple())))
